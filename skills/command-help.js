a2b27baef5177c5bfc8358435b20427458faca62
Ribot Sanahuja <oriol.ribot@bt.com>
2018-03-01 15:00:26 +0100

    cambios espacio

--- skills/command-help.js
+++ skills/command-help.js
@@ -7,6 +7,7 @@ module.exports = function (controller) {
         var text = "Here are my skills:";
         text += "\n- " + bot.enrichCommand(message, ".commons") + ": shows metadata about myself";
         text += "\n- " + bot.enrichCommand(message, "help") + ": spreads the word about my skills";
+        text += "\n- " + bot.enrichCommand(message, "espacio") + ": copy the room";
         bot.reply(message, text);
     });
 }
